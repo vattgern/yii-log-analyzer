8f879369898c022b1809f9b6c6e6f833957658ad
vattgern <sashawot060@gmail.com>
2026-05-19 13:09:11 +0300

    console command to import data

---
+++ commands/LogController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\commands;
+
+use app\components\LogParser;
+use app\models\Log;
+use yii\console\Controller;
+use yii\console\ExitCode;
+
+class LogController extends Controller
+{
+    /**
+     * Импортировать данные из логов
+     */
+    public function actionImport(string $filePath)
+    {
+        if (!file_exists($filePath)) {
+            $this->stderr("Файл не найден: {$filePath}\n");
+            return ExitCode::UNSPECIFIED_ERROR;
+        }
+
+        $fp = fopen($filePath, 'r');
+        if (!$fp) {
+            $this->stderr("Невозможно открыть файл: {$filePath}\n");
+            return ExitCode::UNSPECIFIED_ERROR;
+        }
+
+        $success = 0;
+        $failed = 0;
+
+        while (($line = fgets($fp)) !== false) {
+            $data = LogParser::parse($line);
+            if ($data) {
+                $log = new Log();
+                $log->attributes = $data;
+
+                if ($log->save())
+                    $success++;
+                else
+                    $failed++;
+            } else {
+                $failed++;
+            }
+
+            if (($success + $failed) % 1000 === 0)
+                $this->stdout("Processed: " . ($success + $failed) . "\n");
+        }
+
+        fclose($fp);
+
+        $this->stdout("Операция завершина\n Успешно: {$success}\n Провалено: {$failed}\n");
+        return ExitCode::OK;
+    }
+}
